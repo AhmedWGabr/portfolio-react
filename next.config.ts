afa3dc305b0aa5f18213ea4756000cd211c27350
AhmedWGabr <5320002@pua.edu.eg>
2025-05-08 21:42:31 +0300

    s

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,7 @@ const nextConfig = {
     ],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; img-src 'self' data: https://placehold.co; script-src 'none'; connect-src 'self'",
+    unoptimized: true,
   },
   output: 'export',
 };
