7b2ede536d03b8e84b56c0337ed9a53c34d904bf
AhmedWGabr <5320002@pua.edu.eg>
2025-05-08 21:18:13 +0300

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,7 @@ const nextConfig = {
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; img-src 'self' data: https://placehold.co; script-src 'none'; connect-src 'self'",
   },
+  output: 'export',
 };
 
 export default nextConfig;
