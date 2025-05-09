d2a574de8592d0a4c92df315774667a3c9c98dfc
AhmedWGabr <5320002@pua.edu.eg>
2025-05-09 11:38:11 +0300

    .

--- next.config.ts
+++ next.config.ts
@@ -11,8 +11,6 @@ const nextConfig = {
     contentSecurityPolicy: "default-src 'self'; img-src 'self' data: https://placehold.co; script-src 'none'; connect-src 'self'",
     unoptimized: true,
   },
-  output: 'export',
-  basePath: '/portfolio-react', // Add the repository name as the base path
 };
 
 export default nextConfig;
